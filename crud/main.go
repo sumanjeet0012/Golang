ca2c71b018ae397b38d513f957beb63e4bd8dfc4
Sumanjeet <sumanjeet0012@gmail.com>
2024-09-15 21:16:53 +0530

    put and delete

--- crud/main.go
+++ crud/main.go
@@ -98,3 +98,71 @@ func main() {
 	fmt.Println("Response is", string(data))
 
 }
+
+func performUpdateRequest() {
+	todo := Todo{
+		UserID:    23789,
+		Title:     "Prince Kumar Golang hello World",
+		Completed: false,
+	}
+
+	// Convert the Todo struct to JSON
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("Error marshalling : ", err)
+		return
+	}
+
+	// convert json data to string
+	jsonString := string(jsonData)
+
+	// convert string data to reader
+	jsonReader := strings.NewReader(jsonString)
+
+	const myurl = "https://jsonplaceholder.typicode.com/todos/1"
+
+	// create PUT Request
+
+	req, err := http.NewRequest(http.MethodPut, myurl, jsonReader)
+	if err != nil {
+		fmt.Println("Error creating PUT Request : ", err)
+		return
+	}
+	req.Header.Set("Content-type", "application/json")
+
+	// Send the request
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request : ", err)
+		return
+	}
+	defer res.Body.Close()
+
+	data, _ := ioutil.ReadAll(res.Body)
+	fmt.Println("Response : ", string(data))
+	fmt.Println("Response  status : ", res.Status)
+}
+
+func performDeleteRequest() {
+	const myurl = "https://jsonplaceholder.typicode.com/todos/1"
+
+	// create DELETE Request
+
+	req, err := http.NewRequest(http.MethodDelete, myurl, nil)
+	if err != nil {
+		fmt.Println("Error creating Delete Request : ", err)
+		return
+	}
+
+	// Send the request
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request : ", err)
+		return
+	}
+	defer res.Body.Close()
+
+	fmt.Println("Response  status : ", res.Status)
+}
